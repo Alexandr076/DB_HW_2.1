7b781299ee6417dc8b919f43fe16f8739c8f2789
Aleksandr Zhdankov <alexandr0751@yandex.ru>
2023-10-09 22:49:56 +0300

    init

---
+++ script.sql
@@ -0,0 +1,22 @@
+CREATE TABLE CUSTOMERS (
+    id SERIAL PRIMARY KEY,
+    name varchar,
+    surname varchar,
+  	age integer,
+  	phone_number varchar
+ );
+ 
+ CREATE TABLE ORDERS (
+    id SERIAL PRIMARY KEY,
+    date varchar,
+    customer_id integer,
+  	product_name integer,
+  	amount varchar,
+	CONSTRAINT fk_customer
+      FOREIGN KEY(customer_id) 
+	  REFERENCES customers(id)
+ );
+ 
+select product_name
+from ORDERS o join customers c on o.customer_id = c.id
+where UPPER(c.name) = 'alexey'
